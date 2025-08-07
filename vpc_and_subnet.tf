c3667de8da925de387008e7b79ba8eeac8a1b0f9
yunseo0406 <lcss0406@gmail.com>
2025-08-07 15:28:05 +0900

    vpc, subnet, acg 설정

---
+++ vpc_and_subnet.tf
@@ -0,0 +1,44 @@
+# VPC 생성
+resource "ncloud_vpc" "yunseo_vpc" {
+  name             = "yunseo_vpc"
+  ipv4_cidr_block  = "10.0.0.0/16"
+}
+
+# Subnet 생성
+resource "ncloud_subnet" "public_subnet" {
+  name             = "public_subnet"
+  vpc_no           = ncloud_vpc.yunseo_vpc.id
+  subnet           = "10.0.1.0/24"
+  zone             = "KR-2"
+  subnet_type      = "PUBLIC"
+  usage_type       = "GEN"
+  network_acl_no   = ncloud_vpc.yunseo_vpc.default_network_acl_no
+}
+
+# ACG 생성
+resource "ncloud_access_control_group" "acg" {
+    name = "public-acg"
+    vpc_no = ncloud_vpc.yunseo_vpc.id
+}
+
+resource "ncloud_access_control_group_rule" "acg-rule" {
+  access_control_group_no = ncloud_access_control_group.acg.id
+  
+  inbound {
+    protocol = "TCP"
+    ip_block = "0.0.0.0/0"
+    port_range = "80"
+  }
+
+  inbound {
+    protocol    = "ICMP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = ""
+  }
+
+  outbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "1-65535"
+  }
+}
